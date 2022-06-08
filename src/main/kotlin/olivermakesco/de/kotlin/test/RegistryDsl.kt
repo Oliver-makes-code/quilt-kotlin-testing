386ca95eba68c1c627ef839e89b0acfcbda85019
oliver <kookooloopdaking@gmail.com>
2022-06-07 23:03:04 -0500

    Document

--- src/main/kotlin/olivermakesco/de/kotlin/test/RegistryDsl.kt
+++ src/main/kotlin/olivermakesco/de/kotlin/test/RegistryDsl.kt
@@ -1,34 +1,49 @@
 package olivermakesco.de.kotlin.test
 
-import net.minecraft.block.Block
-import net.minecraft.block.Material
 import net.minecraft.util.Identifier
 import net.minecraft.util.registry.Registry
-import org.quiltmc.qsl.block.extensions.api.QuiltBlockSettings
 
-class RegistryDsl(val modid: String, action: RegistryDsl.() -> Unit) {
-    fun <T> Registry<T>.register(name: String, t: T) {
-        Registry.register(this, Identifier(modid, name), t)
-    }
+
+/**
+ * RegistryDsl class, used for inputting a mod id before registering
+ * @param modid The modid to register under
+ * @param action The DSL action
+ *
+ * @author Oliver-makes-code (Emma)
+ * */
+class RegistryDsl(private val modid: String, action: RegistryDsl.() -> Unit) {
     init {
         apply(action)
     }
+
+    /**
+     * Used for registering an object, uses the mod id provided in the constructor
+     * @param name The name to register under
+     * @param t The object to register
+     *
+     * @author Oliver-makes-code (Emma)
+     * */
+    fun <T> Registry<T>.register(name: String, t: T) {
+        Registry.register(this, Identifier(modid, name), t)
+    }
 }
+/**
+ * Base registry DSL, allows you to call the register functions one after another
+ * @param action The DSL action
+ *
+ * @author Oliver-makes-code (Emm
+ * */
 inline operator fun <reified T> Registry<T>.invoke(action: Registry<T>.() -> Unit) {
     apply(action)
 }
+
+/**
+ * Used for registering an object, uses the mod id provided in the constructor
+ * @param identifier the Identifier to register under
+ * @param t The object to register
+ *
+ * @author Oliver-makes-code (Emma)
+ * */
 fun <T> Registry<T>.register(identifier: Identifier, t: T) {
     Registry.register(this, identifier, t)
-}
-
-fun test() {
-    RegistryDsl("modid") {
-        Registry.BLOCK {
-            register("test_1", Block(QuiltBlockSettings.of(Material.STONE)))
-        }
-    }
-    Registry.BLOCK {
-        register(Identifier("modid","test_2"), Block(QuiltBlockSettings.of(Material.STONE)))
-    }
-    Registry.BLOCK.register(Identifier("modid","test_3"), Block(QuiltBlockSettings.of(Material.STONE)))
 }
